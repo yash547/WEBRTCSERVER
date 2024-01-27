46ae1b2efcdc4d16addfe0e9ec0fe7294e00559d
yash mishra <46112945+yash547@users.noreply.github.com>
2024-01-27 13:05:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http")
 const Socket = require("websocket").server
 const server = http.createServer(()=>{})
 
-server.listen(9982,"192.168.43.208",()=>{
+server.listen(9982,()=>{
     
 })
 
